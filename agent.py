43584f17b8592643e19cdb68e9f36241c8b909f0
bachdx-1327 <duong.xuan.bach@sun-asterisk.com>
2024-05-21 16:39:21 +0700

    feat(init_agent): Adding init agent

--- agent.py
+++ agent.py
@@ -31,3 +31,27 @@ def _is_termination_msg(message):
             contain_code = True
             break 
     return not contain_code
+
+
+def initialize_agents():
+    assistant = AssistantAgent(
+        name = "assistant",
+        max_consecutive_auto_reply=5,
+        llm_config={
+            "timeout": TIMEOUT, 
+            "config_list":config_list,
+        }
+    )
+
+    userproxy = UserProxyAgent(
+        name="userproxy", 
+        human_input_mode="NEVER",
+        is_termination_msg=_is_termination_msg,
+        max_consecutive_auto_reply=5, 
+        code_execution_config={
+            "work_dir": "coding", 
+            "use_docker":False,
+        }
+    )
+
+    return assistant, userproxy
